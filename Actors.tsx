00f04fe52a185225f86abe3ed04f06b78e8c6ce1
mixelon <mixel@me.com>
2026-05-11 02:29:16 +0100

    first commit

---
+++ Actors.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="actors" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+  <image source="actors.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="p1food" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="p1shadow" type="string" />
+    </properties>
+  </tile>
+</tileset>
---
+++ Template.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="templates" tilewidth="256" tileheight="256" tilecount="256" columns="256">
+  <image source="templates.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="p1food" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="p1shadow" type="string" />
+    </properties>
+  </tile>
+</tileset>
